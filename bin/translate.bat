8d1ec826b283ab92afd6a0fc8e557130d5d6c9c4
Wilson Gomes <92630855+developerwill@users.noreply.github.com>
2025-07-19 18:50:18 +0000

    Feature - Multiple language package support (#8)
    * Added support to work with multiple language installed
    
    * Fix UTF-8 decoding issue
    
    * Fix issue windows folder explorer showing behind the terminal

--- bin/translate.bat
+++ bin/translate.bat
@@ -5,8 +5,9 @@ reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 set "SCRIPT_DIR=%~dp0"
 call "%SCRIPT_DIR%argos-env\Scripts\activate.bat"
 
-:: Install tkinter if missing
+:: Install required Python packages
 python -c "import tkinter" || python -m pip install tk
+python -c "import chardet" || python -m pip install chardet
 
 python "%SCRIPT_DIR%..\lib\srt-translator.py"
 pause
